271dc78b6c670222bc34ec8fd6f6bcf36ac7a2a2
Michal Gebski <gebski.michal@gmail.com>
2023-04-25 16:47:07 +0200

    Initial commit

---
+++ src/test/java/pl/sda/micgeb/springthymeleaf/SpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.micgeb.springthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
